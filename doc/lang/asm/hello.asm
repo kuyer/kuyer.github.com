531e39d512334189108b4dba8bea3f07b634f61e
rory.zhang <coolrl@126.com>
2017-06-26 19:51:16 +0800

    update

--- doc/lang/asm/hello.asm
+++ doc/lang/asm/hello.asm
@@ -2,16 +2,17 @@
     global _start
 
     section .data
-    message db "hello, asm!"
+    message db "hello, asm. i love it.", 0xA
+    .length equ $ - message
 
     section .text
 _start:
     mov rax, 1
-    mov rdx, 13
+    mov rdx, message.length
     mov rsi, message
     mov rdi, 1
     syscall
 
-    mov rax, 60
+    mov rax, 60 
     mov rdi, 0
     syscall
